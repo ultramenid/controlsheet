237e499a1cea4e5b3b9e539a0e2ff4b82e960d0b
ultramenid <malichamdan@gmail.com>
2025-03-09 19:06:13 +0700

    hash on update password

--- app/Livewire/EditUserComponent.php
+++ app/Livewire/EditUserComponent.php
@@ -35,7 +35,7 @@ class EditUserComponent extends Component
                 'name' => $this->name,
                 'email' => $this->email,
                 'contact' => $this->contact,
-                'password' => $this->password,
+                'password' => Hash::make($this->password) ?? $this->userPassword,
                 'role_id' => $this->level,
                 'updated_at' => Carbon::now('Asia/Jakarta')
             ]);
