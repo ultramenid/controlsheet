4263b894b7c501cc0fe6059b1352d5490fede746
ultramenid <malichamdan@gmail.com>
2025-03-10 17:31:49 +0700

    margin top on alerts auditor

--- resources/views/alerts.blade.php
+++ resources/views/alerts.blade.php
@@ -4,7 +4,7 @@
     @include('partials.header')
     @include('partials.nav')
 
-    <div class="max-w-6xl mx-auto px-7 py-4 sm:mt-16 mt-4">
+    <div class="max-w-6xl mx-auto px-7 py-4 mt-6">
         <livewire:auditor-database-component />
 
     </div>
